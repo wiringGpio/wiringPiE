f2f1829e383fd9aa599c03bcae151a066c7a0b2b
Graham Briggs <GrahamBriggs@littlebytesofpi.com>
2025-05-24 11:13:22 -0700

    Update wiringPi.c
    Removed redundant hardware check

--- wiringPi/wiringPi.c
+++ wiringPi/wiringPi.c
@@ -50,6 +50,9 @@
  //	02 May 2012:
  //		Added in the 2 UART pins
  //		Change maxPins to numPins to more accurately reflect purpose
+ //	22 May 2025
+ //		Removed redundant "Hardware" check to be more compatible with newer boards
+ //		
 
 
 #include <stdio.h>
@@ -772,61 +775,6 @@ int piGpioLayout(void)
 
 	if ((cpuFd = fopen("/proc/cpuinfo", "r")) == NULL)
 		piGpioLayoutOops("Unable to open /proc/cpuinfo");
-
-	// Start by looking for the Architecture to make sure we're really running
-	//	on a Pi. I'm getting fed-up with people whinging at me because
-	//	they can't get it to work on weirdFruitPi boards...
-
-	while (fgets(line, 120, cpuFd) != NULL)
-		if (strncmp(line, "Hardware", 8) == 0)
-			break;
-
-	if (strncmp(line, "Hardware", 8) != 0)
-		piGpioLayoutOops("No \"Hardware\" line");
-
-	if (wiringPiDebug)
-		printf("piGpioLayout: Hardware: %s\n", line);
-
-	// See if it's BCM2708 or BCM2709 or the new BCM2835.
-
-	// OK. As of Kernel 4.8,  we have BCM2835 only, regardless of model.
-	//	However I still want to check because it will trap the cheapskates and rip-
-	//	off merchants who want to use wiringPi on non-Raspberry Pi platforms - which
-	//	I do not support so don't email me your bleating whinges about anything
-	//	other than a genuine Raspberry Pi.
-
-#ifdef	DONT_CARE_ANYMORE
-	if (!(strstr(line, "BCM2708") || strstr(line, "BCM2709") || strstr(line, "BCM2835")))
-	{
-		fprintf(stderr, "Unable to determine hardware version. I see: %s,\n", line);
-		fprintf(stderr, " - expecting BCM2708, BCM2709 or BCM2835.\n");
-		fprintf(stderr, "If this is a genuine Raspberry Pi then please report this\n");
-		fprintf(stderr, "at GitHub.com/wiringPi/wiringPi. If this is not a Raspberry Pi then you\n");
-		fprintf(stderr, "are on your own as wiringPi is designed to support the\n");
-		fprintf(stderr, "Raspberry Pi ONLY.\n");
-		exit(EXIT_FAILURE);
-	}
-#endif
-
-	// Actually... That has caused me more than 10,000 emails so-far. Mosty by
-	//	people who think they know better by creating a statically linked
-	//	version that will not run with a new 4.9 kernel. I utterly hate and
-	//	despise those people.
-	//
-	//	I also get bleats from people running other than Raspbian with another
-	//	distros compiled kernel rather than a foundation compiled kernel, so
-	//	this might actually help them. It might not - I only have the capacity
-	//	to support Raspbian.
-	//
-	//	However, I've decided to leave this check out and rely purely on the
-	//	Revision: line for now. It will not work on a non-pi hardware or weird
-	//	kernels that don't give you a suitable revision line.
-
-	// So - we're Probably on a Raspberry Pi. Check the revision field for the real
-	//	hardware type
-	//	In-future, I ought to use the device tree as there are now Pi entries in
-	//	/proc/device-tree/ ...
-	//	but I'll leave that for the next revision. Or the next.
 
 	// Isolate the Revision line
 
